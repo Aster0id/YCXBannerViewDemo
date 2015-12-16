64ad806cb07bad3db7edc5101182c43019ca6b09
Aster0id <aster0id@sina.com>
2015-12-16 10:54:32 +0800

    add YCXBannerView

--- YCXBannerViewDemo/ViewController.m
+++ YCXBannerViewDemo/ViewController.m
@@ -9,14 +9,20 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation ViewController
+{
+    NSArray *_list;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _list = @[@"本地图片",@"网络图片"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +30,26 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _list.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.textLabel.text = _list[indexPath.row];
+    return cell;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSLog(@"%@", indexPath);
+    
+}
 @end
---
+++ YCXBannerViewDemo/YCXBannerView/YCXBannerPhoto.h
@@ -0,0 +1,13 @@
+//
+//  YCXBannerPhoto.h
+//  YCXBannerViewDemo
+//
+//  Created by 牛萌 on 15/12/16.
+//  Copyright © 2015年 self.Aster0id. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCXBannerPhoto : NSObject
+
+@end
